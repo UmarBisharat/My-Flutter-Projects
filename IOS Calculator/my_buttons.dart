b62be59d6efea41de006d43c7401d0f030a31857
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-03-19 06:05:47 +0500

    Create my_buttons.dart

---
+++ IOS Calculator/my_buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String title;
+  final Color color;
+  final VoidCallback onPress;
+  MyButton({
+    super.key,
+    required this.title,
+    this.color = const Color(0xffa5a5a5),
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: InkWell(
+          onTap: onPress,
+          child: Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
